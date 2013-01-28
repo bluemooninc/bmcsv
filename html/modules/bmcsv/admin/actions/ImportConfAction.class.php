aed6336bcff6f537cb1834f0b14e8885c39a16bb
Yoshi Sakai <yoshi.sakai@gmail.com>
2013-01-28 18:03:30 +0900

    Kick off

---
+++ html/modules/bmcsv/admin/actions/ImportConfAction.class.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * Copyright(c): Bluemoon inc.
+ * Author : Yoshi Sakai
+ * Date: 2013/01/28
+ * Time: 13:14
+ * To change this template use File | Settings | File Templates.
+ */
+
+if (!defined('XOOPS_ROOT_PATH')) exit();
+
+require_once dirname(__FILE__) . "/ImportAction.class.php";
+require_once XOOPS_MODULE_PATH . "/bmcsv/admin/class/ImportCsv.php";
+
+class bmcsv_ImportConfAction extends bmcsv_ImportAction
+{
+	protected $mKey = array();
+	protected $mModuleName;
+	protected $mTableName;
+	protected $mHandler;
+	protected $mPrimaryKey;
+
+	function &_getHandler()
+	{
+		$this->mModuleName = xoops_getrequest("moduleName");
+		$this->mTableName = xoops_getrequest("tableName");
+		$this->mHandler = xoops_getmodulehandler($this->mTableName,$this->mModuleName);
+		return $this->mHandler;
+	}
+
+	function execute()
+	{
+		$this->_getHandler();
+
+		/// csv file check
+		if (isset($_FILES['bmcsv_csv_file']) &&
+			$_FILES['bmcsv_csv_file']['error'] == 0){
+			return BMCSV_FRAME_VIEW_SUCCESS;
+		}
+		return $this->getDefaultView();
+	}
+
+
+	function executeViewSuccess(&$controller,&$render)
+	{
+		// success
+		$render->setTemplateName("csv_import_conf.html");
+
+		// csv data
+		$csvData = array();
+		$csvFName = XOOPS_TRUST_PATH . "/cache/".time().".csv";
+		@move_uploaded_file( $_FILES['bmcsv_csv_file']['tmp_name'], $csvFName );
+
+		// Get table keys
+		$object = $this->mHandler->create();
+		$this->mKey = array_keys($object->gets());
+
+		// Set key to restore
+		if (function_exists('mb_detect_encoding')){
+			$csv_encoding = "SJIS-WIN";
+		}else{
+			$csv_encoding = '';
+		}
+		$lineCount=0;
+		$fp = fopen($csvFName, 'r');
+		$iCsv = new importCsv($this->mKey);
+		$this->mPrimaryKey = $iCsv->mysql_get_prim_key($this->mModuleName."_".$this->mTableName);
+		$iPrimary = $iCsv->getPrimaryKeyPosition( $this->mPrimaryKey, $this->mKey);
+		while (!feof($fp)) {
+			$_line = $iCsv->loadCSV($fp, $csv_encoding);
+			if ($lineCount > 0 && $_line) {
+				$csvData[] = $iCsv->loadOneLineToArray($_line, $iPrimary, $this->mKey, $this->mHandler);
+			}
+			$lineCount++;
+		}
+		fclose($fp);
+		$render->setAttribute('import_fields', $this->mKey);
+		$render->setAttribute('csv_data', $csvData, $iCsv->mObjects() );
+		$render->setAttribute('lineCount', $lineCount-1 );
+		$render->setAttribute('uploaded_filename', $csvFName);
+		$render->setAttribute('moduleName',$this->mModuleName );
+		$render->setAttribute('tableName', $this->mTableName );
+	}
+}
---
+++ html/modules/bmcsv/language/ja_utf8/admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * Copyright(c): Bluemoon inc.
+ * Author : Yoshi Sakai
+ * Date: 2013/01/28
+ * Time: 17:57
+ * To change this template use File | Settings | File Templates.
+ */
+define("_AM_BMCSV_TITLE", "B.M.CSV");
+define("_AM_BMCSV_MODULE_NAME", "モジュール名称");
+define("_AM_BMCSV_TABLE_NAME", "テーブル名称");
+define("_AM_BMCSV_CTRL_BLOCK", "操作ブロック");
